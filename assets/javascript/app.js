f0f181b23b9a0a873d84e8a1b5c1047a3c336f4b
imitchell717 <56371676+imitchell717@users.noreply.github.com>
2019-11-22 20:43:39 -0500

    I started a form with id of my text and questions after I applied p for each question I asked also containing inputs with radio buttons to create multiple choice questions for all questions with their own value for answewrs to each question After i styled my css with a body containing my Lato font after inside my javascript i did a function check adding my questions also the correct value then i applied if statements for each question to equal its own answer to be able to add 1 to the users score that theyve got correct I also added my after

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,4 +1,20 @@
 function check(){
 
-    alert("")
+var question1 = document.quiz.question1.value;
+var question2 = document.quiz.question2.value;
+var question3 = document.quiz.question3.value;
+var correct = 0;
+
+    if (question1 == "King Ossie II") {
+        correct++;
+}
+    if (question2 == "Ghana in 1952") {
+        correct++;
+}
+    if (question3 == "Culture") {
+        correct++;
+    }
+
+document.getElementById("after_submit").style.visibility = "visible";
+document.getElementById("number_correct").innerHTML = "You got " + correct + " correct.";
 }
