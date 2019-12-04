0fd16eebb5ff378658d03044b8928a99d87d9b79
imitchell717 <56371676+imitchell717@users.noreply.github.com>
2019-12-03 22:12:11 -0500

    adding files

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -8,34 +8,34 @@ var correct = 0;
     if (question1 == "King Ossie II"); {
         correct++;
 }
-    if (question2 == "Ghana in 1952"); {
+    if (question2 == "Ghana in 1952") {
         correct++;
 }
-    if (question3 == "Culture"); {
+    if (question3 == "Culture") {
         correct++;
     }
 
 var messages = ["Oh You Think You Lit", "You Almost Won It All", "You Really Need To Read More"];
 
-var pictures = ["img/lostOdog.gif"];
+var pictures = ["assets/images/yourighPac.gif", "assets/images/youfdup.gif", "assets/images/lostOdog.gif"];
 
-var range;
+var score;
 
     if (correct < 1) {
-        range = 2;
+        score = 2;
     }
 
     if (correct > 0 && correct < 3) {
-        range = 1;
+        score = 1;
     }
 
     if (correct > 2) {
-        range = 0;
+        score = 0;
     }
 
 document.getElementById("after-submit").style.visibility = "visible";
 
-document.getElementById("messages").innerHTML = messages[range];
+document.getElementById("messages").innerHTML = messages[score];
 document.getElementById("number-correct").innerHTML = "You got " + correct + " correct";
-document.getElementById("picture").src = pictures[range];
+document.getElementById("picture").src = pictures[score];
 }
